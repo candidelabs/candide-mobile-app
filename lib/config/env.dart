9249e1edcf5c7ddf2d6f3c94b1174114f48f33ef
Andrew Wahid <andrew.wahid1@gmail.com>
2022-11-04 22:54:59 +0200

    add boolean testnet

--- lib/config/env.dart
+++ lib/config/env.dart
@@ -1,17 +1,17 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class Env {
+  static late bool testnet;
   static late String explorerUri;
   static late String bundlerUri;
-  static late String legacyBundlerUri;
   static late String securityUri;
   static late String magicApiKey;
   static late String nodeRpcEndpoint;
 
   static initialize() async {
     await dotenv.load(fileName: ".env");
+    testnet = dotenv.get('NETWORK_ENV', fallback: 'testnet') == "testnet";
     explorerUri = dotenv.get('EXPLORER_URL', fallback: 'http://192.168.1.3:3000');
-    legacyBundlerUri = dotenv.get('LEGACY_BUNDLER_URL', fallback: 'http://192.168.1.3:3002');
     bundlerUri = dotenv.get('BUNDLER_URL', fallback: 'http://192.168.1.3:3002');
     securityUri = dotenv.get('SECURITY_URL', fallback: 'http://192.168.1.3:3004');
     magicApiKey = dotenv.get('MAGIC_API_KEY', fallback: '-');
