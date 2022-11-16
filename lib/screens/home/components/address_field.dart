b1c36dae3b8d1d029dbcc8a028e5ddbf076413e4
Andrew Wahid <andrew.wahid1@gmail.com>
2022-11-16 04:08:26 +0200

    Support all ens TLDs

--- lib/screens/home/components/address_field.dart
+++ lib/screens/home/components/address_field.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:blockies/blockies.dart';
 import 'package:candide_mobile_app/screens/components/continous_input_border.dart';
 import 'package:candide_mobile_app/screens/home/components/address_qr_scanner.dart';
@@ -26,142 +28,159 @@ class _AddressFieldState extends State<AddressField> {
   //
   bool _correctAddress = false;
   String address = "";
-  Map? _ensResponse = null;
-  bool retrievingENS = false;
   //
-  retrieveENS() async {
-    if (retrievingENS) return;
+  RegExp ensRegex = RegExp(r"^.*\.[A-Za-z]{2,}");
+  String? ensError = "";
+  bool ensTimerActive = false;
+  Timer? ensTimer;
+  Map? _ensResponse;
+  //
+  // Starts/restarts ENS Timer, or stops the timer if regex doesn't match
+  void restartENSListener() async {
+    if (!ensRegex.hasMatch(address)){
+      if (!ensTimerActive) return;
+      setState(() {
+        ensTimer?.cancel();
+        ensError = "";
+        ensTimerActive = false;
+      });
+      return;
+    }
+    ensTimer?.cancel();
+    setState((){
+      ensError = "";
+      ensTimerActive = true;
+      ensTimer = Timer(const Duration(milliseconds: 500), retrieveENS);
+    });
+  }
+
+  void retrieveENS() async {
     if (_ensResponse != null){
       if (_ensResponse!["lastEns"] == address) return;
     }
-    if (address.endsWith(".eth")){
-      retrievingENS = true;
-      var cancelLoad = Utils.showLoading();
-      //
-      final _ensAddress = await Constants.ens.withName(address.toLowerCase()).getAddress();
-      //
-      if (_ensAddress.hex == Constants.addressZeroHex){
-        Utils.showError(title: "ENS Error", message: "ENS domain not found, please make sure that you've typed the ENS correctly");
+    if (!ensRegex.hasMatch(address)) return;
+    //
+    final _ensAddress = await Constants.ens.withName(address.toLowerCase()).getAddress();
+    //
+    if (_ensAddress.hex == Constants.addressZeroHex){
+      setState(() {
+        ensError = "ENS domain not found";
+        _correctAddress = false;
         _ensResponse = null;
-      }else{
-        setState(() {
-          _ensResponse = {};
-          _ensResponse!["address"] = _ensAddress.hex;
-          _ensResponse!["lastEns"] = address;
-          widget.onENSChange(_ensResponse!);
-          _correctAddress = true;
-        });
-      }
-      retrievingENS = false;
-      cancelLoad();
+        ensTimerActive = false;
+      });
+    }else{
+      setState(() {
+        ensError = "";
+        _ensResponse = {};
+        _ensResponse!["address"] = _ensAddress.hex;
+        _ensResponse!["lastEns"] = address;
+        widget.onENSChange(_ensResponse!);
+        _correctAddress = true;
+        ensTimerActive = false;
+      });
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    bool isKeyboardShowing = MediaQuery.of(context).viewInsets.vertical > 0;
-    if (!isKeyboardShowing){
-      retrieveENS();
-    }
     return Column(
       children: [
         Container(
             margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
             alignment: Alignment.centerLeft,
-            child: Focus(
-              onFocusChange: (hasFocus){
-                if (!hasFocus){
-                  retrieveENS();
+            child: TextFormField(
+              controller: _addressController,
+              decoration: InputDecoration(
+                hintText: widget.hint,
+                filled: widget.filled,
+                fillColor: Get.theme.cardColor,
+                border: const ContinousInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(35),)
+                ),
+                suffixIcon: _ensResponse == null ? (!ensTimerActive ? IconButton(
+                  onPressed: (){
+                    showBarModalBottomSheet(
+                      context: context,
+                      builder: (context) {
+                        return AddressQRScanner(
+                          onScanAddress: (_address){
+                            address = _address;
+                            _addressController.text = address;
+                            setState(() {
+                              _correctAddress = true;
+                            });
+                            widget.onAddressChanged(address);
+                          },
+                          alertWidget: widget.qrAlertWidget,
+                        );
+                      },
+                    );
+                  },
+                  icon: const Icon(PhosphorIcons.scanBold),
+                ) : Transform.scale(
+                  scale: 0.3,
+                  child: const CircularProgressIndicator(strokeWidth: 13,)
+                )) : Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween, // added line
+                  mainAxisSize: MainAxisSize.min, // added line
+                  children: [
+                    const Icon(Icons.check_circle_rounded, color: Colors.green,),
+                    IconButton(
+                      onPressed: (){
+                        setState((){
+                          address = "";
+                          _addressController.text = address;
+                          _ensResponse = null;
+                          widget.onENSChange(null);
+                        });
+                      },
+                      icon: const Icon(Icons.close),
+                    )
+                  ],
+                ),
+                prefixIcon: _ensResponse != null ? Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 7, horizontal: 7),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(50),
+                    child: Blockies(
+                      seed: _ensResponse!["address"],
+                      color: Get.theme.colorScheme.primary,
+                      size: 10,
+                    ),
+                  ),
+                ) : null,
+              ),
+              validator: (val) {
+                if (val == null || val.isEmpty) return 'required';
+                if (!Utils.isValidAddress(val) && !ensRegex.hasMatch(address)){
+                  return 'Invalid address';
+                }
+                if (ensError?.isNotEmpty ?? false) return ensError;
+                return null;
+              },
+              onChanged: (val){
+                if (val != address){
+                  if (_ensResponse != null){
+                    setState(() => _ensResponse = null);
+                    widget.onENSChange(null);
+                  }
+                }
+                address = val;
+                restartENSListener();
+                if (!Utils.isValidAddress(address)){
+                  if (_correctAddress){
+                    widget.onAddressChanged(address);
+                    setState(() => _correctAddress = false);
+                  }
+                }else{
+                  if (!_correctAddress){
+                    widget.onAddressChanged(address);
+                    setState(() => _correctAddress = true);
+                  }
                 }
               },
-              child: TextFormField(
-                controller: _addressController,
-                decoration: InputDecoration(
-                  hintText: widget.hint,
-                  filled: widget.filled,
-                  fillColor: Get.theme.cardColor,
-                  border: const ContinousInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(35),)
-                  ),
-                  suffixIcon: _ensResponse == null ? IconButton(
-                    onPressed: (){
-                      showBarModalBottomSheet(
-                        context: context,
-                        builder: (context) {
-                          return AddressQRScanner(
-                            onScanAddress: (_address){
-                              address = _address;
-                              _addressController.text = address;
-                              setState(() {
-                                _correctAddress = true;
-                              });
-                              widget.onAddressChanged(address);
-                            },
-                            alertWidget: widget.qrAlertWidget,
-                          );
-                        },
-                      );
-                    },
-                    icon: const Icon(PhosphorIcons.scanBold),
-                  ) : Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween, // added line
-                    mainAxisSize: MainAxisSize.min, // added line
-                    children: [
-                      const Icon(Icons.check_circle_rounded, color: Colors.green,),
-                      IconButton(
-                        onPressed: (){
-                          setState((){
-                            address = "";
-                            _addressController.text = address;
-                            _ensResponse = null;
-                            widget.onENSChange(null);
-                          });
-                        },
-                        icon: const Icon(Icons.close),
-                      )
-                    ],
-                  ),
-                  prefixIcon: _ensResponse != null ? Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 7, horizontal: 7),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(50),
-                      child: Blockies(
-                        seed: _ensResponse!["address"],
-                        color: Get.theme.colorScheme.primary,
-                        size: 10,
-                      ),
-                    ),
-                  ) : null,
-                ),
-                validator: (val) {
-                  if (val == null || val.isEmpty) return 'required';
-                  if (!Utils.isValidAddress(val) && !val.endsWith(".eth")){
-                    return 'Invalid address';
-                  }
-                  return null;
-                },
-                onChanged: (val){
-                  if (val != address){
-                    if (_ensResponse != null){
-                      setState(() => _ensResponse = null);
-                      widget.onENSChange(null);
-                    }
-                  }
-                  address = val;
-                  if (!Utils.isValidAddress(address)){
-                    if (_correctAddress){
-                      widget.onAddressChanged(address);
-                      setState(() => _correctAddress = false);
-                    }
-                  }else{
-                    if (!_correctAddress){
-                      widget.onAddressChanged(address);
-                      setState(() => _correctAddress = true);
-                    }
-                  }
-                },
-                autovalidateMode: AutovalidateMode.onUserInteraction,
-              ),
+              autovalidateMode: AutovalidateMode.onUserInteraction,
             )
         ),
         SizedBox(height: _ensResponse != null ? 5 : 0,),
