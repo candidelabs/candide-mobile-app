ed6eb99cbece462bad6a4a7e7a490dbdfa709728
Marc Sednaoui <marcsednaoui@hotmail.com>
2022-12-05 11:40:20 +0100

    add public address on overview page (with copy ability)

--- lib/screens/home/overview_screen.dart
+++ lib/screens/home/overview_screen.dart
@@ -12,9 +12,11 @@ import 'package:candide_mobile_app/screens/home/components/deposit_sheet.dart';
 import 'package:candide_mobile_app/screens/home/components/network_bar.dart';
 import 'package:candide_mobile_app/screens/home/send/send_sheet.dart';
 import 'package:candide_mobile_app/utils/events.dart';
+import 'package:candide_mobile_app/utils/utils.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
+import 'package:phosphor_flutter/phosphor_flutter.dart';
 import 'package:pull_to_refresh_flutter3/pull_to_refresh_flutter3.dart';
 
 
@@ -28,6 +30,7 @@ class OverviewScreen extends StatefulWidget {
 class _OverviewScreenState extends State<OverviewScreen> {
   final RefreshController _refreshController = RefreshController(initialRefresh: true);
   late final StreamSubscription transactionStatusSubscription;
+  final ownerPublicAddress = AddressData.wallet.walletAddress.hexEip55;
   bool balancesVisible = true;
 
   fetchOverview() async {
@@ -76,13 +79,27 @@ class _OverviewScreenState extends State<OverviewScreen> {
           Row(
             children: [
               const SizedBox(width: 10,),
-              Text("Assets", style: TextStyle(fontFamily: AppThemes.fonts.gilroyBold, fontSize: 25),),
+              Icon(PhosphorIcons.user, size: 25, color: Get.theme.colorScheme.primary),
+              const SizedBox(width: 10,),
+              Text(
+                Utils.truncate(ownerPublicAddress, trailingDigits: 3), 
+                style: TextStyle(fontSize: 18, fontFamily: AppThemes.fonts.gilroy),
+              ),
+              const SizedBox(width: 10,),
+              IconButton(
+                splashRadius: 12,
+                style: const ButtonStyle(
+                  tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                  visualDensity: VisualDensity.compact,
+                ),
+                onPressed: () => Utils.copyText(ownerPublicAddress, message: "Address copied to clipboard!"),
+                icon: Icon(PhosphorIcons.copyLight, color: Get.theme.colorScheme.primary , size: 20),
+              ),
               const Spacer(),
               NetworkBar(network: Networks.get(SettingsData.network)!),
               const SizedBox(width: 10,),
             ],
           ),
-          const SizedBox(height: 20,),
           BalanceCard(
             balanceVisible: balancesVisible,
             onPressVisibilityIcon: () async {
@@ -95,7 +112,7 @@ class _OverviewScreenState extends State<OverviewScreen> {
                 context: context,
                 builder: (context) => SingleChildScrollView(
                   controller: ModalScrollController.of(context),
-                  child: DepositSheet(address: AddressData.wallet.walletAddress.hexEip55),
+                  child: DepositSheet(address: ownerPublicAddress),
                 ),
               );
             },
